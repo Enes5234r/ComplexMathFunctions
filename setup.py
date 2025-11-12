6840c6653704254c4c7e68e57ec8528dab8f0153
Enes5234r <99439043+Enes5234r@users.noreply.github.com>
2025-11-12 20:24:54 +0300

    v0.4

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "complexmathfunctions",
+    name = "ComplexMathFunctions",
     version = "0.1",
     author = "Enes523r",
     author_email = "bayintinkercadhesabi@gmail.com",
-    packages = find_packages,
-    description = "This library facilitates the calculation of mathematical definitions/functions that require lengthy processes using code.",
-    keywords = ["difficult_maths"]
+    packages = find_packages
 )
