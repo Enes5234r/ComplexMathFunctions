56908e1e36bd8dc282a50e691debb09b067bc838
Enes5234r <99439043+Enes5234r@users.noreply.github.com>
2025-11-12 20:46:13 +0300

    v0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "ComplexMathFunctions",
-    version = "0.1",
+    version = "0.6",
     author = "Enes523r",
     author_email = "bayintinkercadhesabi@gmail.com",
     packages = find_packages(include=['ComplexMathFunctions', 'ComplexMathFunctions.difficult_maths.py'])
